b74c48f171d17cb37cd39b253baff39c72707f15
Luis Corales <luisaugustocorales@hotmail.com>
2022-10-29 22:40:06 -0500

    Check position and insert mark

--- game.py
+++ game.py
@@ -1,11 +1,31 @@
-board = ["o", "x", "o", "x", "x", "o", "o", "o", "x"]
+board = {1: ' ', 2: ' ', 3: ' ',
+         4: ' ', 5: ' ', 6: ' ',
+         7: ' ', 8: ' ', 9: ' '}
+
 
 def printBoard(board):
-    print(board[0] + "|" + board[1] + "|" + board[2])
+    print(board[1] + "|" + board[2] + "|" + board[3])
     print("-+-+-")
-    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[4] + "|" + board[5] + "|" + board[6])
     print("-+-+-")
-    print(board[6] + "|" + board[7] + "|" + board[8])
+    print(board[7] + "|" + board[8] + "|" + board[9])
     print("\n")
+
+
+def isPositionFree(pos):
+    if board[pos] == " ":
+        return True
+    else:
+        return False
+
+
+def insertMark(mark, pos):
+    if isPositionFree(pos):
+        board[pos] = mark
+        return
+    else:
+        print("The position is not free")
+        return
+
 
 printBoard(board)
